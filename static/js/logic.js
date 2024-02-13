55ad84251809a9e2c1c9a7491a9b4d880c0972e0
TinaDhn <Tina.Dhn@gmail.com>
2024-02-12 22:02:29 -0800

    leaflet

---
+++ static/js/logic.js
@@ -0,0 +1,99 @@
+// Store our API endpoint as queryUrl.
+var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+
+// Perform a GET request to the query URL.
+d3.json(queryUrl).then(function(data) {
+  // Once we get a response, send the data.features object to the createFeatures function.
+  createFeatures(data.features);
+});
+
+function createFeatures(earthquakeData) {
+  // Define the getColor function to assign color based on depth.
+  function getColor(depth) {
+    if (depth < 10) {
+      return "#ffffcc";
+    } else if (depth < 30) {
+      return "#a1dab4";
+    } else if (depth < 50) {
+      return "#41b6c4";
+    } else if (depth < 70) {
+      return "#2c7fb8";
+    } else if (depth < 90) {
+      return "#253494";
+    } else {
+      return "#081d58";
+    }
+  }
+
+  // Define a function that we want to run once for each feature in the features array.
+  // Give each feature a popup that describes the place, magnitude, and depth of the earthquake.
+  function onEachFeature(feature, layer) {
+    layer.bindPopup(`<h3>${feature.properties.place}</h3><hr><p>Magnitude: ${feature.properties.mag}</p><p>Depth: ${feature.geometry.coordinates[2]}</p>`);
+  }
+
+  // Create a GeoJSON layer that contains the features array on the earthquakeData object.
+  // Run the onEachFeature function once for each piece of data in the array.
+  var earthquakes = L.geoJSON(earthquakeData, {
+    pointToLayer: function(feature, latlng) {
+      return L.circleMarker(latlng, {
+        radius: Math.sqrt(feature.properties.mag) * 5,
+        fillColor: getColor(feature.geometry.coordinates[2]),
+        color: "#000",
+        weight: 1,
+        opacity: 1,
+        fillOpacity: 0.8
+      });
+    },
+    onEachFeature: onEachFeature
+  });
+
+  // Send our earthquakes layer to the createMap function.
+  createMap(earthquakes);
+}
+
+function createMap(earthquakes) {
+  // Create the base layers.
+  var street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  });
+
+  // Create a baseMaps object.
+  var baseMaps = {
+    "Street Map": street
+  };
+
+  // Create an overlay object to hold our overlay.
+  var overlayMaps = {
+    Earthquakes: earthquakes
+  };
+
+  // Create our map, giving it the streetmap and earthquakes layers to display on load.
+  var myMap = L.map("map", {
+    center: [37.09, -60],
+    zoom: 1.5,
+    layers: [street, earthquakes]
+  });
+
+  // Create a layer control.
+  // Pass it our baseMaps and overlayMaps.
+  // Add the layer control to the map.
+  L.control.layers(baseMaps, overlayMaps,{
+    collapsed: false
+  }).addTo(myMap);
+
+  // Add legend
+  var legend = L.control({position: "bottomright"});
+  legend.onAdd = function() {
+    var div = L.DomUtil.create("div", "info legend"),
+    depth = [-10, 10, 30, 50, 70, 90];
+    
+    div.innerHTML += "<h3 style='text-align: center'>Depth</h3>"
+    for (var i =0; i < depth.length; i++) {
+      div.innerHTML += 
+      '<i style="background:' + getColor(depth[i] + 1) + '"></i> ' +
+          depth[i] + (depth[i + 1] ? '&ndash;' + depth[i + 1] + '<br>' : '+');
+    }
+    return div;
+  };
+  legend.addTo(myMap);
+}
